4679a2ada5fe322884f67c1926f4aa73c7602331
rajibmiah <ahmedrajib942@gmail.com>
2021-12-11 09:33:39 +0600

    project init

---
+++ quiz/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    pass
+
+class Quizzes(models.Model):
+    pass
+
+class Question():
+    pass
+
+class Answer():
+    pass
